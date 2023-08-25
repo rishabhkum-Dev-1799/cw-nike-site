7267df3f1b024fc7ffdbef251c99574b46fd2f14
rishabhkum-Dev-1799 <rishabhkum17@gmail.com>
2023-08-25 20:10:19 +0530

    "feature-state management in the hero section"

--- src/components/ShoeCard.jsx
+++ src/components/ShoeCard.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 
-const ShoeCard = ({thumbNail,onClick}) => {
+const ShoeCard = ({thumbNail,onClick,selectedShoe}) => {
   return (
-    <div onClick={onClick} className=' relative flex justify-center items-center rounded-lg sm:w-[40] sm:h-[40] max-sm:p-4 border-2 bg-card bg-center bg-cover cursor-pointer hover:-translate-y-1  hover:transition-all z-20'>
+    <div onClick={onClick} className={` relative flex justify-center items-center rounded-lg sm:w-[40] sm:h-[40] max-sm:p-4 border-2 bg-card bg-center bg-cover cursor-pointer hover:-translate-y-1  hover:transition-all z-20 ${selectedShoe && 'border-coral-red '}`}>
         <img src={thumbNail} alt='img' width={127} height={103} className='object-contain'  />
     </div>
   )
--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -10,14 +10,17 @@ import RatingItem from "../components/RatingItem";
 
 const Hero = () => {
   const [imageState, setImageState] = useState(bigShoe1);
+  const clickHandler=(event,shoe)=>{
+    setImageState((prevValue)=>shoe.bigShoe);
+  }
   return (
-    <section className="max-container w-full min-h-screen flex flex-col lg:flex-row gap-10 justify-center">
-      <section className="relative  w-full xl:w-2/5 flex flex-col items-start justify-center pt-28  ">
+    <section className="max-container w-full min-h-screen  flex flex-col lg:flex-row gap-10 justify-center">
+      <section className="relative  w-full xl:w-2/5 flex flex-col items-start justify-center pt-28 max-xl:padding-x  ">
         <p className="font-montserrat text-2xl text-coral-red">
           {englishLang.heroSubtitle}
         </p>
         <h1 className=' mt-10 font-palanquin text-8xl font-bold max-sm:text-[72px] max-sm:leading-[82px]'>
-          <span className='xl:bg-white xl:whitespace-nowrap relative z-10 pr-10 '>The New Arrival</span> <span className="text-coral-red inline-block mt-3">Nike</span> Shoes</h1>
+          <span className='xl:bg-white xl:whitespace-nowrap relative z-10 pr-10 '>The New Arrival</span> <br/> <span className="text-coral-red inline-block mt-3">Nike</span> Shoes</h1>
         <p className="info-text mt-6 mb-14 sm:max-w-sm">{englishLang.heroDescription}</p>
         <div>
           <PrimaryButton >
@@ -31,12 +34,13 @@ const Hero = () => {
             ))}
       </div>
       </section>
-      <section className="relative flex-1 flex items-center justify-center max-sm:py-40  bg-hero bg-center bg-cover">
-        <img src={imageState} alt="nike" className="relative z-10" />
+      <section className="relative flex-1 flex items-center justify-center xl:min-h-screen max-sm:py-40  bg-hero bg-center bg-cover">
+        <img src={imageState} alt="nike" width={610} height={500} className="relative z-10" />
         <div className="flex sm:gap-6 gap-4 absolute -bottom-[8%] sm:left-[10%] max-sm:p-6">
           {shoes.map((shoe, index) => (
             <ShoeCard
               thumbNail={shoe.thumbnail}
+              selectedShoe={imageState===shoe.bigShoe?true:false}
               onClick={(event) => clickHandler(event, shoe)}
               key={index}
             />
