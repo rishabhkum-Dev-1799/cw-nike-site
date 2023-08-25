72bb9bc4f1b9340b543a450e681bdbad62bb6dc4
rishabhkum-Dev-1799 <rishabhkum17@gmail.com>
2023-08-25 11:20:33 +0530

    "ui modifications-Service card translation actions"

--- src/components/ServiceCard.jsx
+++ src/components/ServiceCard.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const ServiceCard = ({imgURL,label,subtext}) => {
   return (
-    <section className=' border  flex flex-col flex-1 px-10 py-16 rounded-[20px] sm:w-[350px] sm:max-w-[350px] shadow-2xl w-full'>
+    <section className=' border  flex flex-col flex-1 px-10 py-16 rounded-[20px] sm:w-[350px] sm:max-w-[350px] shadow-2xl w-full hover:-translate-y-10 hover:transition-all'>
         <div className='w-11 h-11 bg-coral-red flex items-center justify-center overflow-hidden rounded-full'>
             <img src={imgURL} alt={label} width={24} height={24}/>
         </div>
